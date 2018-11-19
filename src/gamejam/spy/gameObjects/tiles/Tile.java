c2c6f5b7b47d9b4019ff517c3bcfc67d725e790e
James <james@hawkinsplace.co.uk>
2018-11-19 19:36:49 +0000

    set up tiles laminas

--- src/gamejam/spy/gameObjects/tiles/Tile.java
+++ src/gamejam/spy/gameObjects/tiles/Tile.java
@@ -1,9 +1,12 @@
 package gamejam.spy.gameObjects.tiles;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import gamejam.spy.Vector;
 import gamejam.spy.gameObjects.Grid;
+import gamejam.spy.gameObjects.collision.Lamina2D;
+import gamejam.spy.gameObjects.collision.Vertex;
 import gamejam.spy.interfaces.Renderable;
 import gamejam.spy.interfaces.Textured;
 
@@ -12,13 +15,26 @@ public class Tile implements Renderable, Textured {
 	public Vector gridPosition;
 	public Grid parentGrid;
 	public String textureKey;
-	
+	public Lamina2D lamina;
+	public Tile () {
+		
+	}
 	public void setGrid(Grid g) {
 		parentGrid = g;
+
+		
 	}
 	
 	public void setGridPosition(Vector position) {
 		gridPosition = position.copy();
+		ArrayList<Vertex> vertices = new ArrayList<>();
+		vertices.add(new Vertex(parentGrid.tileSize, 0));
+		vertices.add(new Vertex(0, 0));
+		vertices.add(new Vertex(parentGrid.tileSize, parentGrid.tileSize));
+		vertices.add(new Vertex(0, parentGrid.tileSize));
+		lamina = new Lamina2D(vertices, true);
+		lamina.addX(position.getX() * parentGrid.tileSize);
+		lamina.addY(position.getY() * parentGrid.tileSize);
 	}
 	
 	public void tick() {
