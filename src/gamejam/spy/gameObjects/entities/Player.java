57f5f2fb76b92e1ce4f5147e129261f7a0772890
James <james@hawkinsplace.co.uk>
2018-11-19 21:23:13 +0000

    Added right movement animation

--- src/gamejam/spy/gameObjects/entities/Player.java
+++ src/gamejam/spy/gameObjects/entities/Player.java
@@ -1,25 +1,30 @@
 package gamejam.spy.gameObjects.entities;
 
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Collection;
 
 import gamejam.spy.SpyGame;
-import gamejam.spy.Vector;
 import gamejam.spy.controllers.KeyInput;
-import gamejam.spy.controllers.MouseInput;
+import gamejam.spy.controllers.TextureMap;
 import gamejam.spy.gameObjects.collision.Lamina2D;
 import gamejam.spy.gameObjects.collision.Vertex;
 import gamejam.spy.gameObjects.tiles.Tile;
 
 public class Player extends Entity {
 	
-	public String[] textures = {"player", "PlayerRunning1.png", "playerRun2.png"}; // TODO: correct this
-	
+	public String[] textures = {"playerRunning1", "playerRunning2", "playerRunning3", "playerRunning4", "playerRunning5", "playerRunning6", "playerRunning7"}; // TODO: correct this
+	public String idle = "player";
 	private Vertex pos;
 	private Lamina2D lamina;
 	
+	private int counter = 0;
+	private int currentTex = 0;
+	
 	private double yv = 0;
+	
+	boolean isRight = true;
 	
 	public Player() {
 		super();
@@ -33,7 +38,7 @@ public class Player extends Entity {
 		
 		lamina = new Lamina2D(vertices, false);
 		
-		this.setTextureKey(textures[0]);
+		this.setTextureKey(idle);
 	}
 	
 	public void moveX(double x) {
@@ -47,10 +52,26 @@ public class Player extends Entity {
 	public void tick() {
 		super.tick();
 		if (KeyInput.isDown(KeyEvent.VK_D)) {
+			isRight = true;
 			moveX(5);
+			if (counter > 3) {
+				this.setTextureKey(textures[currentTex]);
+				counter -= 3;
+				currentTex = (currentTex + 1) % textures.length;
+			}
+		} else {
+			this.setTextureKey(idle);
+			counter = 0;
+			currentTex = 0;
 		}
 		if (KeyInput.isDown(KeyEvent.VK_A)) {
+			isRight = false;
 			moveX(-5);
+			if (counter > 3) {
+				this.setTextureKey(textures[currentTex]);
+				counter -= 3;
+				currentTex = (currentTex + 1) % textures.length;
+			}
 		}
 		if (KeyInput.isDown(KeyEvent.VK_W) && yv == 0) {
 			this.yv = 10;
@@ -65,7 +86,15 @@ public class Player extends Entity {
 			}
 		}
 		position.setLocation(pos.getX()-4, pos.getY());
-		
+		counter += 1;
+	}
+	
+	public void render(Graphics g) {
+		if (isRight) {
+			g.drawImage(TextureMap.getTexture(getTextureKey()), position.getIX(), position.getIY(), null);
+		} else {
+			
+		}
 	}
 	
 }
