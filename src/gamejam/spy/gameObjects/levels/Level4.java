39406568889f14bd4950a0244f7d3da9908d264d
Laurence <hii488@btinternet.com>
2018-11-20 07:19:14 +0000

    Ensures the music doesn't run out

--- src/gamejam/spy/gameObjects/levels/Level4.java
+++ src/gamejam/spy/gameObjects/levels/Level4.java
@@ -2,6 +2,7 @@ package gamejam.spy.gameObjects.levels;
 
 import gamejam.spy.SpyGame;
 import gamejam.spy.Vector;
+import gamejam.spy.controllers.SoundPlayer;
 import gamejam.spy.gameObjects.Level;
 import gamejam.spy.gameObjects.entities.MovingPlatformVertical;
 import gamejam.spy.gameObjects.entities.Player;
@@ -14,6 +15,8 @@ public class Level4 extends Level{
 
 	public Level4() {
 		super();
+
+		SoundPlayer.playSound("SPY2_SEWER.wav");
 		
 		//Universal Death line
 		for(int i = 0; i < 32; i++) {
